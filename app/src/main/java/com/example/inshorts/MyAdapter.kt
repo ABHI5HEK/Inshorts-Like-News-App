7bbd48fcc45618b93b1868841ad16277b31b7b25
ABHI5HEK <abhishekthakur1529@gmail.com>
2021-09-05 11:26:57 +0530

    swipe indication

--- app/src/main/java/com/example/inshorts/MyAdapter.kt
+++ app/src/main/java/com/example/inshorts/MyAdapter.kt
@@ -2,9 +2,11 @@ package com.example.inshorts
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -20,6 +22,7 @@ class MyAdapter(val context:Context, val newsdata: List<Articles>):RecyclerView.
         var description = itemView.findViewById<TextView>(R.id.description)
         var footerLine = itemView.findViewById<TextView>(R.id.footerLine)
         var bottomLayout = itemView.findViewById<LinearLayout>(R.id.linearLayout2)
+        var swipe = itemView.findViewById<TextView>(R.id.swipeUp)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewholder {
@@ -30,19 +33,32 @@ class MyAdapter(val context:Context, val newsdata: List<Articles>):RecyclerView.
 
     override fun onBindViewHolder(holder: MyViewholder, position: Int) {
         var data = newsdata[position]
-        holder.headline.text = data.title
-        holder.description.text = data.description
-        holder.footerLine.text = data.author
-        var news = data.url
-        if (data.urlToImage != null){
-            Glide.with(context).load(data.urlToImage).into(holder.imageView)
-        }
+        if(data != null){
+            holder.headline.text = data.title
+            holder.description.text = data.description
 
-        holder.bottomLayout.setOnClickListener(View.OnClickListener {
-            var intent = Intent(context, Webpage::class.java)
-            intent.putExtra("URL",news)
-            context.startActivity(intent)
-        })
+            holder.footerLine.text = data.author
+            var news = data.url
+            if (data.urlToImage != null){
+                Glide.with(context).load(data.urlToImage).into(holder.imageView)
+            }
+            if (position == 0){
+                var an = AnimationUtils.loadAnimation(context,R.anim.swipe_view)
+                holder.swipe.startAnimation(an)
+                holder.swipe.visibility = View.VISIBLE
+            }else{
+                holder.swipe.visibility = View.GONE
+            }
+
+            holder.bottomLayout.setOnClickListener(View.OnClickListener {
+                var intent = Intent(context, Webpage::class.java)
+                intent.putExtra("URL",news)
+                context.startActivity(intent)
+            })
+        }else{
+            holder.headline.text = "No Internet"
+            holder.description.text = "Please connect to internet"
+        }
     }
 
     override fun getItemCount(): Int {
